392cfe1246f8ea44dd85b3e18190899ce5bc906e
viroben1 <viroben2001@gmail.com>
2026-04-23 11:42:24 -0700

    fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/game-portfolio/', // ✅ ADD THIS LINE
+  base: '/', // ✅ FIXED
   plugins: [
     vue(),
     vueDevTools(),
@@ -17,4 +16,3 @@ export default defineConfig({
     },
   },
 })
-
